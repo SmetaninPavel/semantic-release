6674ee04494d4b760467b3c8d8011d0b10ab5c0e
Pavel Smetanin <>
2020-09-14 11:57:01 +0300

    feat: add root release config

---
+++ release.config.js
@@ -0,0 +1,15 @@
+module.exports = new Promise(async (resolve) => {
+    resolve({
+        verifyConditions: [],
+        verifyRelease: ['@semantic-release/npm', '@semantic-release/git']
+            .map(require)
+            .map(x => x.verifyConditions),
+        plugins: [
+            "@semantic-release/commit-analyzer",
+            "@semantic-release/release-notes-generator",
+            "@semantic-release/npm",
+            "@semantic-release/git"
+        ],
+        branches: ["master", "next"]
+    });
+});
