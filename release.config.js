51227aac8b3ec499387a9da4c4ba50f1fcaaf197
Pavel Smetanin <>
2020-09-15 09:37:07 +0300

    feat: update packages

--- release.config.js
+++ release.config.js
@@ -8,7 +8,8 @@ module.exports = new Promise(async (resolve) => {
             "@semantic-release/commit-analyzer",
             "@semantic-release/release-notes-generator",
             "@semantic-release/npm",
-            "@semantic-release/git"
+            "@semantic-release/git",
+            "@semantic-release/github",
         ],
         branches: ["master", "next", {name: "beta", prerelease: true}]
     });
